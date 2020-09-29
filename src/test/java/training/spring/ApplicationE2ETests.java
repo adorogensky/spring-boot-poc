2e1d7fc98d2c032273a764220e7ae8a36d7279b6
Alex Dorogensky <alex@exebar.com>
2020-09-29 16:20:30 -0500

    create new ApplicationE2ETests as a copy of ApplicationTests. ApplicationTests: add mvc unit test for root endpoint

---
+++ src/test/java/training/spring/ApplicationE2ETests.java
@@ -0,0 +1,13 @@
+package training.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationE2ETests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- src/test/java/training/spring/ApplicationTests.java
+++ src/test/java/training/spring/ApplicationTests.java
@@ -1,13 +1,28 @@
 package training.spring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@SpringBootTest
-class ApplicationTests {
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-	@Test
-	void contextLoads() {
-	}
+@WebMvcTest(Application.class)
+public class ApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void rootEndpointReturnsHello() throws Exception {
+        mockMvc.perform(
+            get("/")
+        ).andExpect(
+            status().isOk()
+        ).andExpect(
+            content().string("Hello!")
+        );
+    }
 }
