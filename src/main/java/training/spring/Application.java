ed4b6603a380005fab6a3482625c0b4c61809ebf
Alex Dorogensky <alex@exebar.com>
2020-09-30 14:56:05 -0500

    Application: configure spring-security to provide access to any username with password '123'

--- src/main/java/training/spring/Application.java
+++ src/main/java/training/spring/Application.java
@@ -2,13 +2,23 @@ package training.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+
 @SpringBootApplication
 @RestController
-public class Application {
+@EnableWebSecurity
+public class Application extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -19,4 +29,15 @@ public class Application {
 		return String.format("Hello %s!", who);
 	}
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(
+			userName -> new User(userName, "123", Collections.emptyList())
+		);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
